05ddcb1313ff2d25c71addbabec22bba4942b960
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-10-22 00:01:58 +0800

    test10

--- resources/views/sidebar_admin.blade.php
+++ resources/views/sidebar_admin.blade.php
@@ -302,7 +302,7 @@ body {
                   </div>
 
                   <div class="dropdown2">
-                    <i class="bi bi-person-circle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">img src="{{ url('userProfile/' . $user->u_profile) }}"</i>
+                    <i img src="{{ url('userProfile/' . $user->u_profile) }}" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
                     
                       <ul class="dropdown-menu" style="">
                         <li><a class="dropdown-item" href="{{ url('/signout') }}">Sign out</a></li>

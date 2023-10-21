496305d260ab7d0b381f91da9a86af78e71734e0
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-10-21 23:35:21 +0800

    test2

--- resources/views/sidebar_admin.blade.php
+++ resources/views/sidebar_admin.blade.php
@@ -1,3 +1,4 @@
+<style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 * {
   margin: 0;
@@ -104,6 +105,8 @@ body {
   width: 100%;
   transition: all 0.5ms ease;
 }
+</style>
+
 <div class="sidebar">
       <div class="logo-details">
         <center class="profile">

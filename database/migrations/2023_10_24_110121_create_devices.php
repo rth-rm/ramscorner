bf75289339fb5e646dcd599581de217497159fdb
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-10-24 15:45:25 +0800

    migration file

--- database/migrations/2023_10_24_110121_create_devices.php
+++ database/migrations/2023_10_24_110121_create_devices.php
@@ -13,17 +13,14 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('devices', function (Blueprint $table) {
-            $table->unsignedBigInteger('d_ID')->autoIncrement();
-            $table->varchar('d_code');
-            $table->varchar('d_name');
+            $table->string('d_ID');
+            $table->string('d_name');
             $table->unsignedBigInteger('d_inventorynum');
             $table->timestamp('d_purchase_date');
-            $table->varchar('d_type');
-            $table->varchar('d_location');
+            $table->string('d_type');
+            $table->string('d_assignment');
             $table->boolean('d_approve')->default("false");
-            $table->varchar('d_submittedby');
-
-
+            $table->string('d_submittedby');
 
             $table->timestamps();
 

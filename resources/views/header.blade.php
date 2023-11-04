32a04b94f46ef44d4e8d4dbe3ce529191d3e5735
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-11-05 03:14:43 +0800

    asset and device

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -29,11 +29,6 @@
             box-sizing: border-box;
             font-family: "Poppins", sans-serif;
         }
-
-        body {
-            min-height: 100vh;
-            width: 100%;
-        }
     </style>
 
 

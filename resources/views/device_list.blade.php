8d31a4281ff1b5ac3dcecc6893224709f47f46b2
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-11-05 16:39:12 +0800

    device

--- resources/views/device_list.blade.php
+++ resources/views/device_list.blade.php
@@ -1,18 +1,9 @@
 @include('header')
 <title>DEVICE LIST | RAMS CORNER</title>
 <link rel="stylesheet" href="{{ asset('assets/css/device-list.css') }}" type = "text/css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 <!-- data tables -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-<!-- bootstrap link -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-</script>
 </head>
-
-
 
 <body>
     @foreach ($user_loggedin as $userloggedin)
@@ -129,21 +120,18 @@
                             </script>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
 
-        </section>
+                    </section>
 
 
-        <!-- sidebar button script -->
-        <script>
-            let sidebar = document.querySelector(".sidebar");
-            let sidebarBtn = document.querySelector(".sidebarBtn");
+                    <!-- sidebar button script -->
+                    <script>
+                        let sidebar = document.querySelector(".sidebar");
+                        let sidebarBtn = document.querySelector(".sidebarBtn");
 
-            sidebarBtn.onclick = function() {
-                sidebar.classList.toggle("active");
-            }
-        </script>
+                        sidebarBtn.onclick = function() {
+                            sidebar.classList.toggle("active");
+                        }
+                    </script>
     @endforeach
     @include('footer')

558d1b2b13bbb0bb64e2f0e298b56dbb09a08daa
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-11-08 21:51:46 +0800

    admin

--- resources/views/admin_open_ticket.blade.php
+++ resources/views/admin_open_ticket.blade.php
@@ -1,28 +1,28 @@
 @include('header')
 
 
-    <title>TICKET</title>
+<title>TICKET</title>
 
 </head>
 
 <body style="background-color: rgb(255, 255, 255); ">
-@section('title', 'All Tickets')
+    @section('title', 'All Tickets')
 
-@include('sweetalert::alert')
-@foreach ($admin as $user)
-    @include('sidebar_admin')
+    @include('sweetalert::alert')
+    @foreach ($admin as $userloggedin)
+        @include('sidebar_admin')
 
-    @include('open_ticket')
-@endforeach
-<script type="text/javascript">
-    function refreshPage() {
-        // check if page has already been refreshed
-        if (!sessionStorage.getItem('refreshed')) {
-            sessionStorage.setItem('refreshed', 'true');
-            location.reload();
+        @include('open_ticket')
+    @endforeach
+    <script type="text/javascript">
+        function refreshPage() {
+            // check if page has already been refreshed
+            if (!sessionStorage.getItem('refreshed')) {
+                sessionStorage.setItem('refreshed', 'true');
+                location.reload();
+            }
         }
-    }
 
-    setTimeout(refreshPage, 1000); // Refresh after 5 seconds
-</script>
-@include('footer')
+        setTimeout(refreshPage, 1000); // Refresh after 5 seconds
+    </script>
+    @include('footer')

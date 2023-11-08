f0ff3759cc7ea073d3a3ed3ebe907f0f2ecde5ef
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-11-08 09:28:53 +0800

    sasas

--- resources/views/admin_reports.blade.php
+++ resources/views/admin_reports.blade.php
@@ -1,16 +1,26 @@
 @include('header')
 
 
-    <title>REPORTS</title>
+<title>REPORTS</title>
 
 </head>
 
 <body style="background-color: rgb(255, 255, 255); ">
 
-@include('sweetalert::alert')
-@foreach ($admin as $user)
-    @include('sidebar_admin')
+    @include('sweetalert::alert')
+    @foreach ($admin as $user)
+        @include('sidebar_admin')
 
-    @include('reports')
-@endforeach
-@include('footer')
+        @include('reports')
+
+
+        <script>
+            let sidebar = document.querySelector(".sidebar");
+            let sidebarBtn = document.querySelector(".sidebarBtn");
+
+            sidebarBtn.onclick = function() {
+                sidebar.classList.toggle("active");
+            }
+        </script>
+    @endforeach
+    @include('footer')

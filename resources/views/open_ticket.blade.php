34e96467616118347d9c4c8137968c849b681b8f
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-11-14 09:06:10 +0800

    open ticket

--- resources/views/open_ticket.blade.php
+++ resources/views/open_ticket.blade.php
@@ -18,7 +18,8 @@
                         <div class="status-display" style="overflow-y: auto; max-height: 55vh; ">
                             <ul style="list-style: none; padding: 10px;">
                                 @foreach ($status as $status_tracking)
-                                    <li style="margin: 10px 0; padding: 10px; border-left: 5px solid #6644A8;">
+                                    <li
+                                        style="margin: 10px 0; padding: 10px; border-left: 5px solid #6644A8;{{ $status_tracking == $last ? 'background-color: #CDB9D9; color: black' : '' }}">
                                         <h5 style="font-weight: 600; color: #6644a8">
                                             {{ $status_tracking->sh_Status }}</h5>
                                         <h5>{{ $status_tracking->sh_datetime }}</h5>
@@ -73,26 +74,6 @@
 
                     @include('chat')
 
-                    {{-- <div class="dropdown">
-                        <div class="icon-container me-3" type="button" data-bs-toggle="dropdown"
-                            data-bs-auto-close="false" aria-expanded="false"
-                            style="display: flex; justify-content: center; align-items: center; height: 40px; width: 40px; background: turquoise; border-radius: 50%;">
-                            <i class="bi bi-chat-left-fill fs-5" style="color: white;"></i>
-                        </div>
-                        <ul class="dropdown-menu">
-                            <div class="border-0" style="width: 30rem; max-height: 30rem;">
-                                <div class="card-header">
-                                    Ticket #N
-                                </div>
-                                <div class="card-body">
-                                    Chat Code in here
-                                </div>
-                                <div class="card-footer text-body-secondary">
-                                    Chat input In here
-                                </div>
-                            </div>
-                        </ul>
-                    </div> --}}
 
                     <!-- update dropup -->
                     <div class="dropdown">
@@ -101,9 +82,11 @@
                             Update
                         </button>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" onclick="statusUpdate('PENDING')">Resource Pending</a></li>
+                            <li><a class="dropdown-item" onclick="statusUpdate('ONGOING')">Ongoing</a></li>
+                            <li><a class="dropdown-item" onclick="statusUpdate('RESOLVED')">Resolve</a></li>
+                            <li><a class="dropdown-item" onclick="statusUpdate('CLOSE')">Close</a></li>
+                            <li><a class="dropdown-item" onclick="statusUpdate('REJECT')">Reject</a></li>
                         </ul>
                     </div>
 
@@ -111,5 +94,222 @@
             </div>
 
         </div>
+        <div class="modal fade" id="statusUpdate" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <form method="POST" enctype="multipart/form-data" action="{{ url('updateTicket/' . $tickets->t_ID) }}">
+
+                @csrf
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+
+                        <div class="modal-body">
+
+                            {{-- First --}}
+
+                            <div class="email-header">
+                                <h3 class="col" style="align-items: center; text-align:Center; "><strong>Update
+                                        Ticket</strong>
+                                </h3>
+                            </div>
+
+
+
+
+                            <div class="row align-items-start" style="padding-left:10%;padding-right:10%">
+
+                                <select class="form-select mb-2" aria-label="Default select example" name="category"
+                                    title="Select appropriate category for the ticket for easier identifying the solution for the problem and assigning of ticket.">
+                                    @if ($tickets->t_category == 'INFRASTRUCTURE')
+                                        <option value="INFRASTRUCTURE" selected>Infrastructure</option>
+                                        <option value="SOFTWARE">Software</option>
+                                    @else
+                                        <option value="INFRASTRUCTURE">Infrastructure</option>
+                                        <option value="SOFTWARE" selected>Software</option>
+                                    @endif
+
+                                </select>
+                            </div>
+
+                            {{-- Second --}}
+                            <div class="row align-items-start" style="padding-left:10%;padding-right:10%">
+
+
+                                <select class="form-select mb-2" aria-label="Default select example" id="urgency"
+                                    name="urgency">
+
+                                    <option value="1" @if (old('1', $tickets->t_urgency) == '1') selected @endif>Critical
+                                    </option>
+                                    <option value="2" @if (old('2', $tickets->t_urgency) == '2') selected @endif>Urgent
+                                    </option>
+                                    <option value="3" @if (old('3', $tickets->t_urgency) == '3') selected @endif>Normal
+                                    </option>
+                                </select>
+                            </div>
+
+                            <div class="row align-items-start" style="padding-left:10%;padding-right:10%">
+
+                                <select class="form-select mb-2" aria-label="Default select example" id="impact"
+                                    name="impact">
+                                    <option value="1" @if (old('1', $tickets->t_impact) == '1') selected @endif>High
+                                    </option>
+                                    <option value="2" @if (old('2', $tickets->t_impact) == '2') selected @endif>Medium
+                                    </option>
+                                    <option value="3" @if (old('3', $tickets->t_impact) == '3') selected @endif>Low
+                                    </option>
+                                </select>
+                            </div>
+
+                            <div class="row align-items-start" style="padding-left:10%;padding-right:10%">
+
+                                <input type="text" readonly value="{{ $tickets->t_priority }}"
+                                    class="form-control mb-2" id="priority" name="priority">
+                            </div>
+
+
+
+
+
+                            <div class="row align-items-start" style="padding-left:10%;padding-right:10%">
+
+                                <select class="form-select mb-2" aria-label="Default select example"
+                                    id="assign_group">
+                                    <option selected="" value="All">All</option>
+                                    <option value="Infrastructure">Infrastructure</option>
+                                    <option value="Software">Software</option>
+
+                                </select>
+                                <br>
+                            </div>
+                            <div class="row align-items-start" style="padding-left:10%;padding-right:10%">
+
+                                <select class="form-select mb-2" aria-label="Default select example" id="assign_to"
+                                    name="assign">
+                                    @if ($tickets->t_assignTo != 'Not Assigned')
+                                        <option selected="" disable="" value="{{ $tickets->t_assignedTo }}">
+                                            {{ $tickets->t_assignedTo }}</option>
+                                        @foreach ($staffs as $staff)
+                                            <option value="{{ $staff->u_name }}" class="{{ $staff->u_division }}">
+                                                {{ $staff->u_name }}</option>
+                                        @endforeach
+                                    @else
+                                        <option selected value="Not Assigned">Not Assigned</option>
+                                        @foreach ($staffs as $staff)
+                                            <option value="{{ $staff->u_name }}" class="{{ $staff->u_division }}">
+                                                {{ $staff->u_name }}</option>
+                                        @endforeach
+                                    @endif
+                                </select>
+                            </div>
+
+
+                            {{-- Fourth --}}
+                            <div class="row align-items-start" style="padding-left:10%;padding-right:10%">
+
+                                <textarea class="form-control mb-2" rows="5" id="message-text" name="message"></textarea>
+                            </div>
+
+
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </div>
+            </form>
+        </div>
     </div>
 </div>
+<script>
+    function statusUpdate(item) {
+        {{-- $('#statusUpdate').modal('show'); --}}
+        var modal = new bootstrap.Modal(document.getElementById('statusUpdate'));
+        {{-- var modalText = document.getElementById('modalText');
+        modalText.innerText = 'You clicked on: ' + item; --}}
+        modal.show();
+
+
+
+    }
+</script>
+
+{{-- impact/urgency/priority script  --}}
+<script>
+    const urgency = document.getElementById("urgency"),
+        impact = document.getElementById("impact"),
+        priority = document.getElementById("priority");
+    urgency.addEventListener('change', updateInputValue);
+    impact.addEventListener('change', updateInputValue);
+
+    function updateInputValue() {
+        const urge = urgency.value,
+            imp = impact.value;
+        if (urge == 1 && imp == 1) {
+            priority.value = 1;
+        } else if (urge == 1 && imp == 2) {
+            priority.value = 2;
+
+        } else if (urge == 1 && imp == 3) {
+            priority.value = 2;
+
+        } else if (urge == 2 && imp == 1) {
+            priority.value = 1;
+
+        } else if (urge == 2 && imp == 2) {
+            priority.value = 2;
+
+        } else if (urge == 2 && imp == 3) {
+            priority.value = 3;
+
+        } else if (urge == 3 && imp == 1) {
+            priority.value = 2;
+
+        } else if (urge == 3 && imp == 2) {
+            priority.value = 2;
+
+        } else if (urge == 3 && imp == 3) {
+            priority.value = 3;
+
+        } else {
+            priority.value = 3;
+
+        }
+
+    }
+</script>
+
+
+{{-- assigngroup script --}}
+<script>
+    $('#assign_group').on('change', function() {
+        var selectedGroup = $(this).val();
+        console.log(selectedGroup);
+
+        if (selectedGroup == "All") {
+            $(".Infrastructure").show();
+            $(".Software").show();
+        } else if (selectedGroup == "Infrastructure") {
+            $(".Infrastructure").show();
+            $(".Software").hide();
+        } else {
+            $(".Infrastructure").hide();
+            $(".Software").show();
+        }
+
+
+    });
+</script>
+
+</div>
+
+
+
+</div>
+
+
+
+<!-- Modal -->

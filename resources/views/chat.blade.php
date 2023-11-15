34ee3514db44d25386f27c22336a96b0d2b9ecaa
rth-rm <rrmorallos.1219@gmail.com>
2023-11-16 02:36:05 +0800

    ui update

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,4 +1,4 @@
-<STYLE>
+<!-- <STYLE>
     #click {
         display: none;
     }
@@ -47,7 +47,7 @@
         position: absolute;
         right: 30px;
         bottom: 0px;
-        max-width: 400px;
+        width: 400px;
         background: #fff;
         border-radius: 15px;
         box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.1);
@@ -69,7 +69,6 @@
         padding: 0 20px;
         font-weight: 500;
         font-size: 20px;
-        /* background: #05E0E9; */
     }
 
     .wrapper .chat-box {
@@ -88,7 +87,6 @@
     .chat-box form .field {
         height: 50px;
         width: 100%;
-        /* margin-top: 20px; */
     }
 
     .chat-box form .field:last-child {
@@ -98,9 +96,6 @@
     form .field button {
         width: 15%;
         height: 20%;
-
-        /* padding-left: 20px; */
-        /* border: 1px solid lightgrey; */
         outline: none;
         border-radius: 25px;
         font-size: 20px;
@@ -162,12 +157,126 @@
     .chat-box form .field button:active {
         transform: scale(0.97);
     }
-</STYLE>
+</STYLE> -->
+<style>
+    .chat-container {
+        max-width: 50rem;
+        max-height: 10rem;
+        overflow-y: auto;
+        border-radius: 8px;
+    }
 
-<div>
+    .chat-message {
+        margin: 10px;
+        padding: 10px;
+        border-radius: 10px;
+        clear: both;
+        word-wrap: break-word;
+    }
+
+    .sender-message {
+        background-color: #05E0E9;
+        color: white;
+        float: right;
+    }
+
+    .received-message {
+        background-color: #4CAF50;
+        color: white;
+        float: left;
+    }
+
+    .card-footer {
+        border-top: 1px solid #ccc;
+    }
+
+    .field {
+        display: flex;
+    }
+
+    textarea {
+        flex: 1;
+        resize: none;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin-right: 10px;
+        padding: 5px;
+    }
+
+    .btn {
+        background-color: #008CBA;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        padding: 8px;
+        cursor: pointer;
+    }
+
+    .btn i {
+        margin-right: 5px;
+    }
+</style>
+<!-- CHAT BUTTON -->
+<div class="chat dropstart dropup">
+    <div class="icon-container ms-3" type="button" data-bs-toggle="dropdown" data-bs-auto-close="false"
+        aria-expanded="false"
+        style="display: flex; justify-content: center; align-items: center; height: 50px; width: 50px; background: turquoise; border-radius: 50%;">
+        <i class="bi bi-chat-left-fill fs-5" style="color: white;"></i>
+    </div>
+    <ul class="dropdown-menu">
+        <div class="border-0" style="width: 40rem; max-height: 40rem;">
+            <div class="card-header">
+                INC{{ $tickets->t_ID }} Chats
+            </div>
+            <div class="card-body">
+                <div class="chat-container">
+                    @if ($chatss == 0)
+                        <p>No conversation available.</p>
+                    @elseif($chatss > 0)
+                        @foreach ($chats as $chatsss)
+                            @if ($chatsss->us_id == $userloggedin->u_ID)
+                                <div class="chat-message sender-message">
+                                    {{ $chatsss->us_id }} | {{ $chatsss->m_content }}
+                                </div>
+                                <div class="chat-message received-message">
+                                    {{ $chatsss->us_id }} | {{ $chatsss->m_content }}
+                                </div>
+                            @endif
+                        @endforeach
+
+                    @endif
+
+                </div>
+
+            </div>
+            <div class="card-footer text-body-secondary">
+                <form id="chatForm" action="{{ url('sendmessages/' . $tickets->t_ID) }}" enctype="multipart/form-data"
+                    method="post">
+                    @csrf
+                    <div class="field textarea">
+                        <textarea id="message" cols="5" rows="2" placeholder="Leave a message here..." required name="message"></textarea>
+                        <button class="btn" type="submit"><i class="bi bi-send"></i>Send</button>
+                    </div>
+                </form>
+            </div>
+            <script>
+                document.getElementById('message').addEventListener('keydown', function(e) {
+                    if (e.key === 'Enter' && !e.shiftKey) {
+                        e.preventDefault(); // Prevents the newline character
+                        document.getElementById('chatForm').submit();
+                    }
+                });
+            </script>
+        </div>
+    </ul>
+</div>
+
+
+
+<!-- <div>
     <input type="checkbox" id="click">
     <label for="click">
-        <i class="material-symbols-outlined">forum</i>
+        <i class="material-symbols-outlined"><i class="bi bi-chat"></i></i>
         <i class="fas fa-times"></i>
     </label>
     <div class="wrapper">
@@ -177,34 +286,64 @@
         </div>
         <div class="chat-box">
             @if ($chatss == 0)
-                <p>No conversation available.</p>
-            @elseif($chatss > 0)
-                @foreach ($chats as $chatsss)
-                    @if ($chatsss->us_id == $userloggedin->u_ID)
-                        <div style = "background-color: #008CBA; color: white; align-self: flex-start;">
-                            {{ $chatsss->us_id }} | {{ $chatsss->m_content }}
-                        </div>
-                    @else
-                        <div style = "background-color: #008CBA; color: white; align-self: flex-end;">
-                            {{ $chatsss->us_id }} | {{ $chatsss->m_content }}
-                        </div>
-                    @endif
-                @endforeach
-
-            @endif
-            <!-- <div class="column" -->
+<p>No conversation available.</p>
+@elseif($chatss > 0)
+@foreach ($chats as $chatsss)
+@if ($chatsss->us_id == $userloggedin->u_ID)
+<div style="background-color: #008CBA; color: white; align-self: flex-start;">
+                {{ $chatsss->us_id }} | {{ $chatsss->m_content }}
+            </div>
+@else
+<div style="background-color: #008CBA; color: white; align-self: flex-end;">
+                {{ $chatsss->us_id }} | {{ $chatsss->m_content }}
+            </div>
+@endif
+@endforeach
+@endif
             <form action="{{ url('sendmessages/' . $tickets->t_ID) }}" enctype="multipart/form-data" method="post">
                 @csrf
                 <div class="field textarea">
-                    <textarea cols="5" rows="4" placeholder="Leave a message here..." required name = "message" id = "message"></textarea>
-                </div>
-                <div class="field">
-                    <!-- <button type="submit">Send Message</button> -->
-                    <button class="btn" type="submit"><i class="material-symbols-outlined"> </i></button>
+                    <textarea cols="5" rows="4" placeholder="Leave a message here..." required name="message" id="message"></textarea>
+                    <button class="btn" type="submit"><i class="bi bi-send"></i></button>
                 </div>
             </form>
-            <!-- </div> -->
         </div>
     </div>
 
+</div> -->
+
+
+<div class="modal fade" id="chatboxModal" tabindex="-1" role="dialog" aria-labelledby="chatboxModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="chatboxModalLabel">Chat</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <div id="chat-messages">
+                    @if ($chatss == 0)
+                        <h5 style="padding: 50px;">No conversation available.</h5>
+                    @elseif($chatss > 0)
+                        @foreach ($chats as $chatsss)
+                            <p>{{ $chatsss->us_id }} | {{ $chatsss->m_content }}</p>
+                        @endforeach
+
+                    @endif
+                </div>
+            </div>
+            <div class="modal-footer">
+                <form id="chat-form" action="{{ url('sendmessages/' . $tickets->t_ID) }}" enctype="multipart/form-data"
+                    method="post">
+                    @csrf
+                    <input type="text" id="message" name="message" class="form-control" placehol
+                        der="Type your message">
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>

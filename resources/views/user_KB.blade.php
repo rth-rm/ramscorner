8327abce3140c722e321163f66875cbfce0436db
rth-rm <rrmorallos.1219@gmail.com>
2023-11-16 07:57:24 +0800

    dev

--- resources/views/user_KB.blade.php
+++ resources/views/user_KB.blade.php
@@ -1,136 +1,121 @@
 @include('header')
-
-
-
-<title>KNOWLEDGE BASE</title>
-
+<title>Knowledge Base - Cliemt| RAMS CORNER</title>
+<link rel="stylesheet" href="{{ asset('assets/css/kb.css') }}" type = "text/css">
+<!-- data tables -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 </head>
 
-<body style="background-color: rgb(255, 255, 255); ">
-    @foreach ($client as $userloggedin)
+<body>
+    @foreach ($user_loggedin as $userloggedin)
+        @include('sweetalert::alert')
         @include('sidebar_client')
-
-
-
-        <!-- 1st -->
-        <div class=""
-            style="text-align: center; border-radius: 4px; border: 1px solid #ffffff7c; position: absolute; background-color: rgb(255, 255, 255); top: 10%; left: 7%; height: 89%; width: 92%; padding: 3%;">
-            <div style="position: relative;">
+        <div class="home-contents">
+            <!-- <div class="dashboard-boxes"></div> -->
+            <div class="title mb-5">
                 <h1>Knowledge Base</h1>
-
-                <form id="search-form">
-                    <input id="search-input" type="search" name="searchKB" placeholder="Search Knowledge Base"
-                        style=" flex: 1; padding: 12px 20px;  margin: 8px 0; box-sizing: border-box;  border: 2px solid #ccc;  border-radius: 4px; width: 70%;">
-                    <button type="submit" value="Search"
-                        style=" width: auto;  padding: 10px 18px;  background-color: #4CAF50; color: white; margin: 8px 0; border: none;  border-radius: 4px; cursor: pointer;">Search</button>
-                </form>
-
-
-                <ul class="nav nav-pills m-3 fs-4" id="pills-tab" role="tablist" style="justify-content: center; ">
-                    <li class="nav-item" role="presentation">
-                        <button onclick="showElement('All')" class="nav-link active" id=""
-                            data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab"
-                            aria-controls="pills-home" aria-selected="true">All</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button onclick="showElement('Infrastructure')" class="nav-link" id="Infrastructure"
-                            data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab"
-                            aria-controls="pills-profile" aria-selected="false">Infrastructure</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <button onclick="showElement('Software')" class="nav-link" id="Software" data-bs-toggle="pill"
-                            data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact"
-                            aria-selected="false">Software</button>
-                    </li>
-                </ul>
+                <i class="bi bi-arrows-expand"></i>
             </div>
 
-            <script>
-                function showElement(className) {
-                    var soft = document.getElementsByClassName('Software');
-                    var infra = document.getElementsByClassName('Infrastructure');
-                    if (className == 'All') {
-                        for (var i = 0; i < soft.length; i++) {
-                            soft[i].style.display = "block";
-                        }
-                        for (var i = 0; i < infra.length; i++) {
-                            infra[i].style.display = "block";
-                        }
-                    }
-                    if (className == 'Infrastructure') {
-                        for (var i = 0; i < soft.length; i++) {
-                            soft[i].style.display = "none";
-                        }
-                        for (var i = 0; i < infra.length; i++) {
-                            infra[i].style.display = "block";
-                        }
-                    }
-                    if (className == 'Software') {
-                        for (var i = 0; i < soft.length; i++) {
-                            soft[i].style.display = "block";
-                        }
-                        for (var i = 0; i < infra.length; i++) {
-                            infra[i].style.display = "none";
-                        }
-                    }
-                }
-            </script>
+            <div class="dash-contents">
+                <div class="dash-container">
+                    <div class="kb-categories me-3">
+                        <div class="kbtitle pt-5; pe-3">
+                            <h3 style="margin-top: 25px; margin-bottom: 25px;">KB Categories</h3>
 
-            <div id="kbs" class="overflow-auto border border-secondary-subtle"
-                style="position: relative; height: 75%; padding: 1%;">
-                <div>
-                    @foreach ($kb_info as $kb_info)
-                        <div class=" {{ $kb_info->kb_category }} kbs-content">
-                            <a href="userkbView/{{ $kb_info->kb_ID }}" class="card content {{ $kb_info->kb_category }}"
-                                id="content clickable"
-                                style="margin: 1%; cursor: pointer; text-decoration:  none; color: black;">
-                                <div class="card-header">
-                                    <h5>{{ 'KBID #' . $kb_info->kb_ID . ': ' . $kb_info->kb_title }}</h5>
-                                </div>
-                                <div class="card-body">
-                                    <blockquote class="blockquote mb-0">
-                                        <p>{{ \Illuminate\Support\Str::limit($kb_info->kb_content, 100) }}</p>
-
-                                        <footer class="blockquote-footer">
-                                            {{ \Illuminate\Support\Str::limit($kb_info->kb_resolution, 50, '...') }}<cite
-                                                title="Source Title"></cite></footer>
-
-                                    </blockquote>
-                                </div>
-                            </a>
                         </div>
-                    @endforeach
+                        <hr>
+                        <div class="kb-btns">
+                            <div class="d-grid gap-1 col-10 mx-auto m-1">
+                                <button class="btn btn-lg" type="button">All</button>
+                                <button class="btn btn-lg" type="button">Approved</button>
+                                <button class="btn btn-lg" type="button">Rejected</button>
+                                <button class="btn btn-lg" type="button">Pending</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="kb-list">
+                        <div class="card kb-list">
+                            <div class="card-body p-0">
+                                <div class="lists">
+                                    <table id="example" class="hover" style="width: 100%">
+                                        <thead>
+                                            <tr>
+                                                <th><i class="bi bi-exclamation-circle"></i></th>
+                                                <th>KBID#</th>
+                                                <th>Title</th>
+                                                <th>Content</th>
+                                                <th>Date Modified</th>
+
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($kb_info as $kb_article)
+                                                <tr onclick="openDiv({{ $kb_article->kb_ID }})">
+                                                    <td>
+
+                                                        @if ($kb_article->kb_status == 'APPROVED')
+                                                            <i class="bi bi-circle-fill" style="color:#05E0E9"></i>
+                                                        @elseif($kb_article->kb_status == 'PENDING')
+                                                            <i class="bi bi-circle-fill" style="color:#EBDDD7"></i>
+                                                        @else
+                                                            <i class="bi bi-circle-fill" style="color:red"></i>
+                                                        @endif
+                                                    </td>
+                                                    <td>{{ $kb_article->kb_ID }}</td>
+                                                    <td>{{ $kb_article->kb_title }}</td>
+                                                    <td>{{ \Illuminate\Support\Str::limit($kb_article->kb_content, 100) }}
+                                                    </td>
+                                                    <td>{{ $kb_article->dateModified }}</td>
+                                                </tr>
+                                                </a>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+                                    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+                                    <script>
+                                        $(document).ready(function() {
+                                            $('#example').DataTable();
+                                        });
+                                    </script>
+                                    <div class="card-footer text-body-secondary"
+                                        style="display: flex; justify-content: end; background-color: #fff">
+                                        <a href = "{{ url('add_kb') }}">
+                                            <button type="button" class="btn btn-primary btn-lg"
+                                                style="border-radius: 25px; background-color:#6644a8;">Create KB
+                                                Article</button>
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
 
-            <script>
-                const searchForm = document.querySelector('#search-form');
-                const searchInput = document.querySelector('#search-input');
-                searchForm.addEventListener('submit', (event) => {
-                    event.preventDefault();
-                    const query = searchInput.value.toLowerCase().trim();
-                    const kbsContentElements = document.querySelectorAll('#kbs .kbs-content');
-                    kbsContentElements.forEach((element) => {
-                        const hasMatch = element.innerText.toLowerCase().includes(query);
-                        if (hasMatch) {
-                            element.style.display = 'block';
-                        } else {
-                            element.style.display = 'none';
-                        }
-                    });
-                });
-                searchInput.addEventListener('input', () => {
-                    const query = searchInput.value.toLowerCase().trim();
-                    const kbsContentElements = document.querySelectorAll('#kbs .kbs-content');
-                    kbsContentElements.forEach((element) => {
-                        const hasMatch = element.innerText.toLowerCase().includes(query);
-                        if (hasMatch) {
-                            element.style.display = 'block';
-                        } else {
-                            element.style.display = 'none';
-                        }
-                    });
-                })
-            </script>
+        </section>
+
+
+        <!-- sidebar button script -->
+        <script>
+            let sidebar = document.querySelector(".sidebar");
+            let sidebarBtn = document.querySelector(".sidebarBtn");
+
+            sidebarBtn.onclick = function() {
+                sidebar.classList.toggle("active");
+            }
+        </script>
+
+
+        <script>
+            function openDiv(divId) {
+                var url = "{{ route('userkbView', '') }}" + "/" + divId;
+                window.location = url;
+            }
+        </script>
+
+        <!-- kb category btn color change script -->
     @endforeach
     @include('footer')

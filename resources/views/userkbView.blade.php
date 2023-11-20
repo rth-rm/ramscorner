a60788e9cb6a2a9f986a8841d27ed49a419ca3de
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-11-20 13:31:57 +0800

    notif

--- resources/views/userkbView.blade.php
+++ resources/views/userkbView.blade.php
@@ -1,38 +1,60 @@
 @include('header')
+<title>Support ID# {{ $kb_info->kb_ID }} </title>
+<link rel="stylesheet" href="{{ asset('assets/css/kb_clicked_admin.css') }}" type="text/css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
-<title> Support ID#{{ $kb_info->kb_ID }}</title>
-<link rel="stylesheet" href="{{ asset('assets/css/client-kb-clicked.css') }}" type="text/css">
 </head>
 
 <body>
-
     @foreach ($client as $userloggedin)
     @include('sidebar_client')
+    @include('sweetalert::alert')
+    <div class="home-contents">
+        <!-- <div class="dashboard-boxes"></div> -->
+        <div class="title mb-5">
+            <h1>Support Hub</h1>
+            <i class="bi bi-arrows-expand"></i>
+        </div>
 
-    <div class="" style=" border-radius: 4px; border: 1px solid #ffffff7c; position: absolute; background-color: rgb(255, 255, 255); top: 10%; left: 7%; height: 89%; width: 92%; padding: 3%;">
-
-        <a href="{{ url()->previous() }}" class="btn bi bi-arrow-left" style="font-size:40px"></a>
-
-        <div class="overflow-auto" style="position: relative; height: 88%;">
-            <div class="card">
-                <div class="card-header" style="background-color:rgb(0, 21, 126); color:white">
-                    <h2 class="text-group-title"><i class="bi bi-list-check" style="font-size:40px"></i>
-                        Support ID#</strong>{{ $kb_info->kb_ID }}:
-                        {{ $kb_info->kb_title }} </h5>
-
-                </div>
-                <div class="card-body">
-                    <div class="text-group-content">
-                        <h6 style="font-size: 24px"><strong>Category:</strong></h6>
-                        <p style="font-size: 20px">{{ $kb_info->kb_category }}</p>
-                        <p style="font-size: 24px"><strong>Content:</strong></p>
-                        <p style="font-size: 20px">{!! nl2br(html_entity_decode($kb_info->kb_content)) !!}</p>
-                        <p style="font-size: 24px"><strong>Resolution:</strong></p>
-                        <p style="font-size: 20px">{!! nl2br(htmlspecialchars($kb_info->kb_resolution)) !!}</p>
+        <div class="dash-contents">
+            <div class="dash-container">
+                <div class="card card-contents">
+                    <div class="cardheader mt-4 ms-5">
+                        <a class="bi bi-arrow-left me-5" href="{{ url()->previous() }}"></a>
+                        <span class="me-5">ID</span>
+                        <span>{{ $kb_info->kb_title }}</span>
                     </div>
+                    <hr>
+                    <div class="card-body p-0 kb-contents ms-5 pe-5">
+                        <i class="bi bi-exclamation-circle me-5"></i>
+                        <span class="me-5">{{ $kb_info->kb_ID }}</span>
+                        <div class="kb-details">
+                            <h3 class="mb-4">{{ $kb_info->kb_category }}</h3>
+                            <h3>Modified by: {{ $kb_info->kb_modify }}</h3>
+                            <h5 class="p-5">
+                                {{ $kb_info->kb_content }}
+                            </h5>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
+
+    </section>
+
+
+    <!-- sidebar button script -->
+    <script>
+        let sidebar = document.querySelector(".sidebar");
+        let sidebarBtn = document.querySelector(".sidebarBtn");
+
+        sidebarBtn.onclick = function() {
+            sidebar.classList.toggle("active");
+        }
+
+    </script>
     @endforeach
     @include('footer')

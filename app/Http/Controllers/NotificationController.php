fce2752582b746696fd0d04eb416f3bfcb6be68c
ruth-morallos <rrmorallos@student.apc.edu.ph>
2023-11-20 12:18:01 +0800

    notif

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -67,7 +67,7 @@ class NotificationController extends Controller
 
         $notif = Notification::where('nID', $nid)->get()->first();
         $user_info = Reporter::where('u_ID', $user->u_ID)->get();
-        $tickets = Ticket::where('t_id', $notif->ticket_id)->get();
+        $tickets = Ticket::where('t_ID', $notif->ticket_id)->get()->first();
         $client = Reporter::where('u_ID', $tickets->u_ID)->get()->first();
         $status = StatusHistory::where('t_id', $tickets->t_ID)->get();
         $staff = Reporter::whereNotIn('u_role', ['Client'])->get();
